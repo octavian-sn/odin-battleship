4fdf0079398e5daefe451e4a21c82546d072481f
Sulic Octavian <octavius1992@gmail.com>
2023-01-11 08:44:22 +0200

    Add attack method to Player and tests

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,4 +1,5 @@
 const Player = (board) => {
+  // Array containing all 100 available attacks
   const attacks = (() => {
     const arr = [];
     for (let i = 0; i < 10; i++) {
@@ -8,15 +9,55 @@ const Player = (board) => {
     }
     return arr;
   })();
+
+  // Array containing attacks recommended after hitting a ship
   const recommendedAttacks = [];
 
-  const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  // Return random number to select a random attack
+  const random = () => Math.floor(Math.random() * ((attacks.length - 1) - 0 + 1) + 0);
+
+  // Return index of attack to remove recommended attack from the 'attacks' array
+  const indxOfAtk = (a, b) => attacks.findIndex((element) => element[0] === a && element[1] === b);
+
+  // Add attacks to recommended list
+  const addRecommended = (x, y) => {
+    if (x - 1 >= 0) recommendedAttacks.push([x - 1, y]);
+    if (x + 1 <= 9) recommendedAttacks.push([x + 1, y]);
+    if (y - 1 >= 0) recommendedAttacks.push([x, y - 1]);
+    if (y + 1 <= 9) recommendedAttacks.push([x, y + 1]);
+  };
 
   return {
+    // How many attacks are left
     attacksAvailable() {
       return attacks.length;
     },
 
+    // Execute an attack
+    attack() {
+      let coordinates;
+
+      // Check if we have recommended attacks or use a random attack
+      if (recommendedAttacks.length > 0) {
+        coordinates = recommendedAttacks[0];
+        recommendedAttacks.shift();
+      } else {
+        coordinates = attacks[random()];
+      }
+
+      // Set coordinates
+      const result = board.receiveAttack(...coordinates);
+      const x = coordinates[0];
+      const y = coordinates[1];
+
+      // If the hit was a success, add the adjacent coordinates to recommended attacks
+      if (result === 1) addRecommended(x, y);
+
+      // Remove the attack from the main attacks array
+      attacks.splice(indxOfAtk(x, y), 1);
+
+      return recommendedAttacks.length;
+    },
   };
 };
 
--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -4,3 +4,17 @@ import GameBoard from '../gameboard';
 test('how many moves are available', () => {
   expect(Player().attacksAvailable()).toEqual(100);
 });
+
+test('check attacks left after executing three attacks', () => {
+  const player = Player(GameBoard());
+  for (let i = 0; i < 3; i++)player.attack();
+  expect(player.attacksAvailable()).toEqual(97);
+});
+
+test('check recommended attacks after executing sixteen attacks', () => {
+  const board = GameBoard();
+  board.randomShips();
+  const player = Player(board);
+  for (let i = 0; i < 15; i++)player.attack();
+  expect(player.attack()).toBeGreaterThan(0);
+});
